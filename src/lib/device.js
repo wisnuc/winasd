6a3ccd75db28be10b4bf30b7628de5cfa0a196ef
CJackYang <cjackyang@qq.com>
2018-11-07 10:50:39 +0800

    update lan ip

---
+++ src/lib/device.js
@@ -0,0 +1,16 @@
+const os = require('os')
+const net = require('net')
+
+const networkInterface = () => {
+  let interfaces = os.networkInterfaces()
+    
+  let keys = Object.keys(interfaces).filter(k => !!k && k !== 'lo')
+  if (!keys.length) return 
+
+  let key = keys.find(k => Array.isArray(interfaces[k]) && interfaces[k].length)
+  if (!key) return
+  let ipv4 = interfaces[key].find(x => x.family === 'IPv4')
+  return ipv4
+}
+
+module.exports.networkInterface = networkInterface
